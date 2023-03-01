fdb293408c6c0427434786fba5814d6d87506b77
KirillBrujah <kirillbrujah@gmail.com>
2023-03-01 17:04:03 +0100

    Some reworks of deals providers logic. Remove listener from all_deals_provider. Update it in deals_fetch_provider

--- lib/providers/all_deals_provider.dart
+++ lib/providers/all_deals_provider.dart
@@ -6,16 +6,19 @@ import 'deals_fetch_provider.dart';
 class AllDealsNotifier extends Notifier<List<DealModel>> {
   @override
   List<DealModel> build() {
-    ref.listen(
-      dealsFetchProvider.select(
-          (AsyncValue<FetchListWrapper> value) => value.asData?.value.data),
-      (previous, next) {
-        if (next == null) return;
-        state = [...state, ...next];
-      },
-    );
-    print("REBUILD ALL DEALS PROVIDER");
+    // ref.listen(
+    //   dealsFetchProvider.select(
+    //       (AsyncValue<FetchListWrapper> value) => value.asData?.value.data),
+    //   (previous, next) {
+    //     if (next == null) return;
+    //     state = [...state, ...next];
+    //   },
+    // );
     return [];
+  }
+
+  void add(Iterable<DealModel> deals) {
+    state = [...state, ...deals];
   }
 
   void remove(DealModel deal) {
--- lib/providers/deals_fetch_provider.dart
+++ lib/providers/deals_fetch_provider.dart
@@ -1,60 +1,31 @@
 import 'dart:async';
+import 'dart:developer';
 
 import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_infinity_scroll_list/models/models.dart';
-import 'package:riverpod_infinity_scroll_list/providers/all_deals_provider.dart';
 import 'package:riverpod_infinity_scroll_list/services/network_service.dart';
 import 'package:riverpod_infinity_scroll_list/utils/debounce.dart';
+
+import 'all_deals_provider.dart';
 
 class AsyncDealsNotifier extends AsyncNotifier<FetchListWrapper<DealModel>> {
   int _page = 0;
   bool _canFetch = true;
 
-  Future<List<DealModel>> _fetch([String? query]) async {
-    final deals = await NetworkService().getDeals(
-      page: _page++,
-      query: query,
-    );
-    _page += 30;
-    _canFetch = deals.isNotEmpty;
-    return deals;
-  }
-
-  final _debounce = Debounce(const Duration(seconds: 1));
-
-  Future<void> search(String query) async {
-    _debounce.run(() async {
-      _page = 0;
-      _canFetch = true;
-      ref.read(allDealsProvider.notifier).refreshList();
-
-      state = const AsyncLoading();
-
-      try {
-        final deals = await _fetch(query);
-        state = AsyncData(FetchListWrapper(data: deals, canFetch: _canFetch));
-      } on DioError catch (error, stackTrace) {
-        if (error.error
-            .toString()
-            .contains("Too Many Results - Refine Search")) {
-          _canFetch = false;
-          state = AsyncData(FetchListWrapper(data: [], canFetch: _canFetch));
-          return;
-        }
-        state = AsyncError(error, stackTrace);
-      } catch (error, stackTrace) {
-        state = AsyncError(error, stackTrace);
-      }
-    });
-  }
-
-  Future<void> fetch() async {
-    if (state.isLoading || !_canFetch) return;
+  Future<void> _fetch([String? query]) async {
     state = const AsyncLoading();
-
     try {
-      final deals = await _fetch();
+      final deals = await NetworkService().getDeals(
+        query: query,
+        page: _page,
+      );
+
+      ref.read(allDealsProvider.notifier).add(deals);
+
+      _page++;
+      _canFetch = deals.isNotEmpty;
+
       state = AsyncData(FetchListWrapper(data: deals, canFetch: _canFetch));
     } on DioError catch (error, stackTrace) {
       if (error.error.toString().contains("Too Many Results - Refine Search")) {
@@ -68,11 +39,45 @@ class AsyncDealsNotifier extends AsyncNotifier<FetchListWrapper<DealModel>> {
     }
   }
 
+  final _debounce = Debounce(const Duration(seconds: 1));
+
+  Future<void> search(String query) async {
+    _debounce.run(() async {
+      _page = 0;
+      _canFetch = true;
+      ref.read(allDealsProvider.notifier).refreshList();
+
+      await _fetch();
+    });
+  }
+
+  Future<void> fetch() async {
+    if (state.isLoading || !_canFetch) return;
+    await _fetch();
+  }
+
   @override
-  Future<FetchListWrapper<DealModel>> build() async {
-    return FetchListWrapper(
-      data: await _fetch(),
-    );
+  FutureOr<FetchListWrapper<DealModel>> build() async {
+    try {
+      final deals = await NetworkService().getDeals(
+        page: _page,
+      );
+
+      ref.read(allDealsProvider.notifier).add(deals);
+
+      _page++;
+      _canFetch = deals.isNotEmpty;
+
+      return FetchListWrapper(
+        data: deals,
+        canFetch: _canFetch,
+      );
+    } catch (e) {
+      log(e.toString(), error: e.toString());
+      return const FetchListWrapper(
+        data: [],
+      );
+    }
   }
 //
 }
